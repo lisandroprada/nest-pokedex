e11a962c1263bf333181a55ee8614e01eef43769
lisandroprada <lisandro.prada@gmail.com>
2023-11-29 13:36:42 -0300

    fin MongoDB Pokedex

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest-pokemon'),
+    PokemonModule,
+    CommonModule,
   ],
   controllers: [],
   providers: [],
